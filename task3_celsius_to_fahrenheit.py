6bc1c7224f77eb63aa47b9e3195ef3e7dfb1ba89
waheed-ali123 <formywork121212@gmail.com>
2026-09-09 22:19:47 +0500

    Add files via upload

---
+++ task3_celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+# Task 3: Take temperature in Celsius as input and convert it to Fahrenheit
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
