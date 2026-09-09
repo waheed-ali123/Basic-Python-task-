6d9e7c87cc41ad72ab5195889679587af17400e6
waheed-ali123 <formywork121212@gmail.com>
2026-09-09 22:20:59 +0500

    Add files via upload

---
+++ task4_power.py
@@ -0,0 +1,9 @@
+# Task 4: Take two numbers as input and calculate the first number
+# raised to the power of the second number (exponentiation)
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = num1 ** num2
+
+print(f"{num1} raised to the power of {num2} is {result}")
