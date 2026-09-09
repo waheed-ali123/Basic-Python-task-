ba54eec29a1b978ec74a123d084e9dd575ef284a
waheed-ali123 <formywork121212@gmail.com>
2026-09-09 22:18:35 +0500

    Add files via upload

---
+++ task2_gross_pay.py
@@ -0,0 +1,8 @@
+# Task 2: Take hours worked and hourly rate as input, calculate and display gross pay
+
+hours = float(input("Enter hours worked: "))
+rate = float(input("Enter hourly rate: "))
+
+gross_pay = hours * rate
+
+print("Gross Pay:", gross_pay)
